d3e7703cee792a7881b794e0cf75347daba2b65d
DeadBread76 <blakeando@live.com>
2019-02-25 09:39:28 +1100

    Add files via upload

--- spammer/messagespam.py
+++ spammer/messagespam.py
@@ -10,13 +10,27 @@ SERVER = sys.argv[2]
 tokenno = sys.argv[3]
 msgtxt = sys.argv[4]
 textchan = sys.argv[5]
+allchan = sys.argv[6]
 
 @client.event
 async def on_ready():
     print ("Token " + str(tokenno) + " logged in!")
     txtchan = client.get_channel(textchan)
-    while True:
-        await client.send_message(txtchan, msgtxt)
+    if allchan == 'true':
+        while not client.is_closed:
+           for c in client.get_server(SERVER).channels:
+                if c.type != discord.ChannelType.text:
+                   continue
+                myperms = c.permissions_for(client.get_server(SERVER).get_member(client.user.id))
+                if not myperms.send_messages:
+                    continue
+                try:
+                    await client.send_message(c, msgtxt)
+                except:
+                    print("Token " + str(tokenno) + ': Error sending message.')
+    else:
+        while True:
+            await client.send_message(txtchan, msgtxt)
 
 client.run(token, bot=False)
 
