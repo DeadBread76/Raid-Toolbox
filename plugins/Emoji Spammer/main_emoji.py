df5b654858d8be6e10fd5cb3d64961704f99bbae
Cahya Darma Wijaya <50060785+lav13enrose@users.noreply.github.com>
2019-05-27 07:33:05 +0800

    Update main_emoji.py

--- plugins/Emoji Spammer/main_emoji.py
+++ plugins/Emoji Spammer/main_emoji.py
@@ -16,7 +16,29 @@ tcounter = 0
 for token in tokenlist:
     tcounter += 1
     number = str(tcounter)
-    p = subprocess.Popen([pyprefix,'plugins/emojispammer/emojispammer.py',token,chan],stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+    p = subprocess.Popen([pyprefix,'# Plugin Author: DeadBread76
+# Plugin Name: tourettes
+# Date: 29/03/2019
+
+import subprocess
+import time
+import sys
+import os
+
+pyprefix = sys.argv[1]
+chan = input("Channel ID: ")
+tokenlist = open("tokens.txt").read().splitlines()
+tcounter = 0
+
+for token in tokenlist:
+    tcounter += 1
+    number = str(tcounter)
+    p = subprocess.Popen([pyprefix,'plugins/Emoji Spammer/emojispammer.py',token,chan],stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+    time.sleep(0.1)
+    with open('pluginpids', 'a+') as handle:
+        handle.write(str(p.pid)+'\n')
+input("Press enter to return to raid toolbox.")
+emojispammer.py',token,chan],stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
     time.sleep(0.1)
     with open('pluginpids', 'a+') as handle:
         handle.write(str(p.pid)+'\n')
